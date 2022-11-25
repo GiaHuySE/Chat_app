dddfd61d48f3812022f3c48e66d357c4178717f3
Huy Tong <tonghuy214@gmail.com>
2022-11-25 16:15:29 +0700

    config firebase.js

--- firebase.js
+++ firebase.js
@@ -7,4 +7,17 @@ import {
   signInWithEmailAndPassword,
 } from "firebase/auth";
 
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+  apiKey: `${fbConfig.apiKey}`,
+  authDomain: `${fbConfig.authDomain}`,
+  projectId: `${fbConfig.projectId}`,
+  storageBucket: `${fbConfig.storageBucket}`,
+  messagingSenderId: `${fbConfig.messagingSenderId}`,
+  appId: `${fbConfig.appId}`,
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 export { auth, createUserWithEmailAndPassword, signInWithEmailAndPassword };
